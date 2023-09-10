492f086800db84a409febd7e000bdb38c02bccb7
Wynn Su <a.wynnsu@gmail.com>
2023-09-10 22:26:28 +0800

    新增 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { MainComponent } from './components/main/main.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    MainComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
